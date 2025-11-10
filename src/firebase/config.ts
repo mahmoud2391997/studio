e906aee9ad4762dce2a86eccb93b5954918a30b7
محمود محمد السيد محمد السيد <mahmoudmelsaid1@gmail.com>
2025-11-10 07:58:39 +0000

    Set up a Firebase backend

---
+++ src/firebase/config.ts
@@ -0,0 +1,8 @@
+export const firebaseConfig = {
+  "projectId": "studio-9900367850-c4e98",
+  "appId": "1:305307802105:web:21e1e0ea130d5fd3eb4528",
+  "apiKey": "AIzaSyAn9mgYvRP5sBgm_wr1LrGngahdZ-gPfwk",
+  "authDomain": "studio-9900367850-c4e98.firebaseapp.com",
+  "measurementId": "",
+  "messagingSenderId": "305307802105"
+};
